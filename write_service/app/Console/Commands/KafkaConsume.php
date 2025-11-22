0f462e6036270a153343469b25898a567093aad3
Mohamed Reda <webzerone@gmail.com>
2025-11-22 23:58:37 +0200

    init

---
+++ write_service/app/Console/Commands/KafkaConsume.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class KafkaConsume extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:kafka-consume';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
--- write_service/routes/console.php
+++ write_service/routes/console.php
@@ -2,7 +2,49 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use RdKafka\Producer;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('kafka:push {message}', function ($message) {
+    $conf = new \RdKafka\Conf();
+    $conf->set('metadata.broker.list', 'kafka:29092');
+
+    $producer = new Producer($conf);
+    $topic = $producer->newTopic('test-topic');
+
+    $topic->produce(RD_KAFKA_PARTITION_UA, 0, $message);
+    $producer->flush(10000);
+
+    $this->info("Message sent to Kafka: $message");
+});
+
+Artisan::command('kafka:consume', function () {
+    $conf = new \RdKafka\Conf();
+    $conf->set('group.id', 'laravel-consumer');
+    $conf->set('metadata.broker.list', 'kafka:29092');
+
+    $consumer = new \RdKafka\KafkaConsumer($conf);
+    $consumer->subscribe(['test-topic']);
+
+    $this->info("Listening for messages...");
+
+    while (true) {
+        $message = $consumer->consume(120*1000);
+        switch ($message->err) {
+            case RD_KAFKA_RESP_ERR_NO_ERROR:
+                $this->info("Received: " . $message->payload);
+                break;
+            case RD_KAFKA_RESP_ERR__PARTITION_EOF:
+                break;
+            case RD_KAFKA_RESP_ERR__TIMED_OUT:
+                $this->warn("Timed out");
+                break;
+            default:
+                $this->error($message->errstr());
+                break;
+        }
+    }
+});
